d5e6a47bc0623e5143eaa45a844c43749a4a4c0c
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2025-01-21 08:13:12 +0100

    feat: various improvements to version import and .gitignore

--- src/ansys/api/platform/instancemanagement/__init__.py
+++ src/ansys/api/platform/instancemanagement/__init__.py
@@ -1,8 +1,5 @@
 """Auto-generated Python gRPC interface package for the ``ansys-api-platform-instancemanagement package``."""
 
-import pathlib
-
 __all__ = ["__version__"]
 
-with open(pathlib.Path(__file__).parent / "VERSION", encoding="utf-8") as f:
-    __version__ = f.read().strip()
+from ._version import version as __version__
---
+++ src/ansys/api/platform/instancemanagement/_version.py
@@ -0,0 +1,3 @@
+"""Version of ansys-api-platform-instancemanagement module."""
+
+__version__ = "1.2.0-dev0"
