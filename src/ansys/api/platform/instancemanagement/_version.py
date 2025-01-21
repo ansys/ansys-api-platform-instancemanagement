317a851c013c6edac4a1ee2611beb30cd4394496
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2025-01-21 09:27:01 +0100

    fix: alias for version

--- src/ansys/api/platform/instancemanagement/_version.py
+++ src/ansys/api/platform/instancemanagement/_version.py
@@ -1,3 +1,7 @@
 """Version of ansys-api-platform-instancemanagement module."""
 
+# Version of the ansys-api-platform-instancemanagement module
 __version__ = "1.2.0-dev0"
+
+# Defining a version alias for compatibility with other modules
+version = __version__
